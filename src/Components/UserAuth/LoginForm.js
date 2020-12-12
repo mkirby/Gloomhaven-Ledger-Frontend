28496efce2101a9aa41649f92f21d4cdfa17d47b
Matthew Kirby <matt.kirby@yahoo.com>
2020-12-12 13:09:30 -0500

    started the LoginForm

---
+++ src/Components/UserAuth/LoginForm.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { ReactReduxContext } from 'react-redux'
+
+class LoginForm extends React.Component {
+  state = {
+
+  }
+
+  changeHandler = () => {
+    
+  }
+
+  render() {
+    return (
+    <form>
+
+    </form>)
+  }
+
+}
+
+export default LoginForm
