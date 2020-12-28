6e5f8a8caecaab2fad8db5b64bab2e8ed5b02a09
Matthew Kirby <matt.kirby@yahoo.com>
2020-12-28 16:47:10 -0500

    updated footer styling adn links

--- src/Main/FooterContainer.js
+++ src/Main/FooterContainer.js
@@ -1,18 +1,50 @@
-import React from 'react'
-import './FooterContainer.css'
+import React from "react";
+import { Button, Icon } from "semantic-ui-react";
+import "./FooterContainer.css";
 
 function FooterContainer() {
   return (
     <footer>
-      <div className='widget-1'>Component: Footer.js</div>
-      <div className='widget-2'>
-        <p>Github</p>
+      <div className="widget-1">
+        Gloomhaven and all related properties, images and text are owned by
+        Cephalofair Games.
       </div>
-      <div className='widget-3'>
-        <p>Github</p>
+      <div className="widget-2">
+        <p>
+          <Button
+            icon
+            labelPosition="left"
+            size="mini"
+            compact
+            basic
+            inverted
+            as="a"
+            href="https://github.com/mkirby/Gloomhaven-Ledger-Frontend"
+            target="_blank"
+          >
+            <Icon name="github" />
+            Frontend
+          </Button>
+          <Button
+            icon
+            labelPosition="left"
+            size="mini"
+            compact
+            basic
+            inverted
+            as="a"
+            href="https://github.com/mkirby/Gloomhaven-Ledger-Backend"
+            target="_blank"
+          >
+            <Icon name="github" />
+            Backend
+          </Button>{" "}
+          <Icon name="copyright outline" fitted /> 2020 Matthew Robbins Kirby.
+          All Rights Reserved
+        </p>
       </div>
-      <div className='widget-4'>Copyright 2020 Matthew Kirby</div>
-    </footer>)
+    </footer>
+  );
 }
 
-export default FooterContainer
+export default FooterContainer;
