62e66ff9f8542551c0811444948592d0badd73ff
Matthew Kirby <matt.kirby@yahoo.com>
2020-12-28 15:27:45 -0500

    styled campaign cards to reduce menu size

--- src/UserProfile/Components/CampaignCard.js
+++ src/UserProfile/Components/CampaignCard.js
@@ -1,60 +1,62 @@
-import React from 'react'
-import dateFormat from 'dateformat';
-import { Menu } from 'semantic-ui-react';
-import { NavLink } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React from "react";
+import dateFormat from "dateformat";
+import { Menu } from "semantic-ui-react";
+import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
 
-import { campaignAction } from '../../_actions/campaignActions'
-import EditModal from '../Modals/EditModal'
+import { campaignAction } from "../../_actions/campaignActions";
+import EditModal from "../Modals/EditModal";
 
 function CampaignCard(props) {
-  const { campaign } = props
-  const { username } = props.user
-  const { id } = campaign
-  const isOwner = campaign.owner.id === props.user.id
+  const { campaign } = props;
+  const { username } = props.user;
+  const { id } = campaign;
+  const isOwner = campaign.owner.id === props.user.id;
 
   return (
-    <div className='index-card campaign-card'>
+    <div className="index-card campaign-card">
       <h3>{campaign.name}</h3>
       <p>Campaign Started: {dateFormat(campaign.created_at, "m/d/yy")}</p>
       <p>Role: {isOwner ? "Owner" : "Player"}</p>
-      {isOwner && 
-        <Menu widths={3} fluid>
+      {isOwner && (
+        <Menu widths={3} fluid size="mini">
           <Menu.Item
             link
-            name='View'
+            name="View"
             as={NavLink}
             to={`/${username}/campaigns/${id}`}
           />
-          <EditModal model='campaign' trigger='menu' campaign={campaign}/>
+          <EditModal model="campaign" trigger="menu" campaign={campaign} />
           <Menu.Item
             link
             name="Delete"
             onClick={() => {
-              props.delete(campaign)
+              props.delete(campaign);
             }}
           />
-        </Menu>}
-      {!isOwner &&
+        </Menu>
+      )}
+      {!isOwner && (
         <Menu widths={3} fluid>
           <Menu.Item
             link
-            name='View'
+            name="View"
             as={NavLink}
             to={`/${username}/campaigns/${id}`}
           />
-        </Menu>}
+        </Menu>
+      )}
     </div>
-  )
+  );
 }
 
 function mapStateToProps(state) {
-  const { user } = state.authentication
-  return { user }
+  const { user } = state.authentication;
+  return { user };
 }
 
 const actionCreators = {
-  delete: campaignAction.deleteCampaign
-}
+  delete: campaignAction.deleteCampaign,
+};
 
-export default connect(mapStateToProps, actionCreators)(CampaignCard)
+export default connect(mapStateToProps, actionCreators)(CampaignCard);
