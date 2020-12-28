e7f8ca1007b8e582afa49b7130f7c979abd8f388
Matthew Kirby <matt.kirby@yahoo.com>
2020-12-28 18:00:23 -0500

    removed outdated reducers for characters, parties, and campaigns

--- src/_reducers/rootReducer.js
+++ src/_reducers/rootReducer.js
@@ -1,18 +1,12 @@
-import { combineReducers } from 'redux'
-import { alert } from './alertReducer'
-import { authentication } from './authenticationReducer'
-import { registration } from './registrationReducer'
-import { character } from './characterReducer'
-import { party } from './partyReducer'
-import { campaign } from './campaignReducer'
+import { combineReducers } from "redux";
+import { alert } from "./alertReducer";
+import { authentication } from "./authenticationReducer";
+import { registration } from "./registrationReducer";
 
 const rootReducer = combineReducers({
   alert: alert,
   authentication: authentication,
   registration: registration,
-  character: character,
-  party: party,
-  campaign: campaign
-})
+});
 
-export default rootReducer
+export default rootReducer;
