9a35d8f756bebf46f7b355bccda67aa3032b790a
Matthew Kirby <matt.kirby@yahoo.com>
2020-12-29 11:16:52 -0500

    removed template headder on create and edit modals

--- src/UserProfile/Modals/CreateModal.js
+++ src/UserProfile/Modals/CreateModal.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import { Header, Modal, Button, Menu } from 'semantic-ui-react'
-import CreateCampaignForm from '../Forms/CreateCampaignForm'
-import CreateCharacterForm from '../Forms/CreateCharacterForm'
-import CreatePartyForm from '../Forms/CreatePartyForm'
+import React from "react";
+import { Header, Modal, Button, Menu } from "semantic-ui-react";
+import CreateCampaignForm from "../Forms/CreateCampaignForm";
+import CreateCharacterForm from "../Forms/CreateCharacterForm";
+import CreatePartyForm from "../Forms/CreatePartyForm";
 
 function CreateModal(props) {
-  const [open, setOpen] = React.useState(false)
+  const [open, setOpen] = React.useState(false);
 
   // required props
-  const { model } = props // what model is being edited? 'character', 'party', 'campaign'
-  const {trigger} = props // what element triggers the modal? 'button', 'menu'
+  const { model } = props; // what model is being edited? 'character', 'party', 'campaign'
+  const { trigger } = props; // what element triggers the modal? 'button', 'menu'
 
-  const uppercasedModel = model.charAt(0).toUpperCase() + model.slice(1)
+  const uppercasedModel = model.charAt(0).toUpperCase() + model.slice(1);
 
-  const handleClose = () => setOpen(false)
+  const handleClose = () => setOpen(false);
 
   return (
     <Modal
@@ -22,36 +22,38 @@ function CreateModal(props) {
       onOpen={() => setOpen(true)}
       open={open}
       trigger={modalTriggerOptions(trigger, uppercasedModel)}
-      size='tiny'
+      size="tiny"
       centered={false}
     >
-      <Modal.Header>Create { uppercasedModel }</Modal.Header>
+      <Modal.Header>Create {uppercasedModel}</Modal.Header>
 
       <Modal.Content>
         <Modal.Description>
-          <Header>Render Different Forms Depending on Model</Header>
-          {model === 'campaign' && <CreateCampaignForm handleClose={handleClose} />}
-          {model === 'party' && <CreatePartyForm handleClose={handleClose} />}
-          {model === 'character' && <CreateCharacterForm handleClose={handleClose} />}
+          {model === "campaign" && (
+            <CreateCampaignForm handleClose={handleClose} />
+          )}
+          {model === "party" && <CreatePartyForm handleClose={handleClose} />}
+          {model === "character" && (
+            <CreateCharacterForm handleClose={handleClose} />
+          )}
         </Modal.Description>
       </Modal.Content>
 
       <Modal.Actions>
         {/* TODO figure out if I can get buttons in here */}
       </Modal.Actions>
-
     </Modal>
-  )
+  );
 }
 
 function modalTriggerOptions(trigger, model) {
-  if (trigger === 'button') {
-    return <Button>Create {model}</Button>
-  } else if (trigger === 'menu') {
-    return (<Menu.Item link name="Create"/>)
+  if (trigger === "button") {
+    return <Button>Create {model}</Button>;
+  } else if (trigger === "menu") {
+    return <Menu.Item link name="Create" />;
   } else {
-    console.log("Unsupported Modal Trigger")
+    console.log("Unsupported Modal Trigger");
   }
 }
 
-export default CreateModal
+export default CreateModal;
--- src/UserProfile/Modals/EditModal.js
+++ src/UserProfile/Modals/EditModal.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import { Header, Modal, Menu, Button } from 'semantic-ui-react'
+import React from "react";
+import { Header, Modal, Menu, Button } from "semantic-ui-react";
 
-import EditCharacterForm from '../Forms/EditCharacterForm'
-import EditPartyForm from '../Forms/EditPartyForm'
-import EditCampaignForm from '../Forms/EditCampaignForm'
+import EditCharacterForm from "../Forms/EditCharacterForm";
+import EditPartyForm from "../Forms/EditPartyForm";
+import EditCampaignForm from "../Forms/EditCampaignForm";
 
 function EditModal(props) {
-  const [open, setOpen] = React.useState(false)
+  const [open, setOpen] = React.useState(false);
 
   // required props
-  const { model } = props // what model is being edited? 'character', 'party', 'campaign'
-  const {trigger} = props // what element triggers the modal? 'button', 'menu'
+  const { model } = props; // what model is being edited? 'character', 'party', 'campaign'
+  const { trigger } = props; // what element triggers the modal? 'button', 'menu'
 
   // required props depending on model
-    // character
-    // party
-    // campaign
-  
-  const uppercasedModel = model.charAt(0).toUpperCase() + model.slice(1)
+  // character
+  // party
+  // campaign
 
-  const handleClose = () => setOpen(false)
+  const uppercasedModel = model.charAt(0).toUpperCase() + model.slice(1);
+
+  const handleClose = () => setOpen(false);
 
   return (
     <Modal
@@ -28,37 +28,46 @@ function EditModal(props) {
       onOpen={() => setOpen(true)}
       open={open}
       trigger={modalTriggerOptions(trigger, uppercasedModel)}
-      size='tiny'
+      size="tiny"
       centered={false}
     >
-      <Modal.Header>Edit { uppercasedModel }</Modal.Header>
+      <Modal.Header>Edit {uppercasedModel}</Modal.Header>
 
       <Modal.Content>
         <Modal.Description>
-          <Header>Render Different Forms</Header>
-          <p>Depending on Model</p>
-          {model === "character" && <EditCharacterForm character={props.character} handleClose={handleClose} />}
-          {model === "party" && <EditPartyForm party={props.party} handleClose={handleClose} />}
-          {model === "campaign" && <EditCampaignForm campaign={props.campaign} handleClose={handleClose} />}
+          {model === "character" && (
+            <EditCharacterForm
+              character={props.character}
+              handleClose={handleClose}
+            />
+          )}
+          {model === "party" && (
+            <EditPartyForm party={props.party} handleClose={handleClose} />
+          )}
+          {model === "campaign" && (
+            <EditCampaignForm
+              campaign={props.campaign}
+              handleClose={handleClose}
+            />
+          )}
         </Modal.Description>
       </Modal.Content>
 
       <Modal.Actions>
         {/* TODO figure out if I can get buttons in here */}
       </Modal.Actions>
-
     </Modal>
-  )
+  );
 }
 
 function modalTriggerOptions(trigger, model) {
-  if (trigger === 'button') {
-    return <Button>Edit {model}</Button>
-  } else if (trigger === 'menu') {
-    return (<Menu.Item link name="Edit"/>)
+  if (trigger === "button") {
+    return <Button>Edit {model}</Button>;
+  } else if (trigger === "menu") {
+    return <Menu.Item link name="Edit" />;
   } else {
-    console.log("Unsupported Modal Trigger")
+    console.log("Unsupported Modal Trigger");
   }
 }
 
-export default EditModal
+export default EditModal;
