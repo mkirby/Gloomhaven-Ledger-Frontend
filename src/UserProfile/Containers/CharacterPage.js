04adaa34c42a376fbb2b955f16591a5a80a223e8
Matthew Kirby <matt.kirby@yahoo.com>
2020-12-29 19:40:07 -0500

    refactored character page to be a functional component

--- src/UserProfile/Containers/CharacterPage.js
+++ src/UserProfile/Containers/CharacterPage.js
@@ -1,151 +1,107 @@
 import React from "react";
 import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
-import { Menu, Placeholder } from "semantic-ui-react";
-import { authHeader } from "../../_helpers/authHeader";
+import { Menu, Segment } from "semantic-ui-react";
 import "./CharacterPage.css";
 
-class CharacterPage extends React.Component {
-  state = {
-    character: {},
-    loaded: false,
-  };
+function CharacterPage(props) {
+  const character = props.user.characters.find(
+    (character) => character.id === parseInt(props.match.params.id)
+  );
+  const { username } = props.user;
 
-  componentDidMount() {
-    fetch(
-      `http://localhost:3000/api/v1/characters/${this.props.match.params.id}`,
-      {
-        headers: authHeader(),
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({ character: data.character, loaded: true });
-      });
-  }
-
-  render() {
-    const { character } = this.state;
-    const { username } = this.props.user;
-    return (
-      <div className="character-page">
-        <div className="character-page-image">
-          {this.state.loaded ? (
+  return (
+    <div className="character-page">
+      {/* character image container */}
+      <div className="character-page__image">
+        {/* loads character image from public folder */}
+        <img
+          src={process.env.PUBLIC_URL + character.character_class.img_portrait}
+          alt={character.character_class.fullname}
+          style={{ width: "100%" }}
+        />
+      </div>
+      {/* all a characters main stats live in this div */}
+      <div className="character-page__stats">
+        {/* header contains the class icons and the character name */}
+        <div className="character-page-header">
+          <div className="character-page-icon">
+            {/* TODO make this a Semantic UI image so I make it's size inline with the character name */}
+            {/* loads class icon image from public folder */}
             <img
-              src={
-                process.env.PUBLIC_URL + character.character_class.img_portrait
-              }
-              alt={character.character_class.fullname}
-              style={{ height: 400, width: 300 }}
+              src={process.env.PUBLIC_URL + character.character_class.img_icon}
+              alt={`${character.character_class.name} icon`}
+              style={{ height: 50, width: 50 }}
             />
-          ) : (
-            <Placeholder style={{ height: 400, width: 300 }}>
-              <Placeholder.Image />
-            </Placeholder>
-          )}
+          </div>
+          {/* div containing character name */}
+          <div className="character-page-name">
+            <h1>{character.name}</h1>
+          </div>
         </div>
-
+        {/* all changable character stats live in this menu */}
         <div>
-          {this.state.loaded && renderCharacterHeader(character)}
-          {this.state.loaded && renderCharacterStats(character)}
-          <div className="character-page-party">
-            {this.state.loaded ? (
-              <h3>
-                Party:{" "}
-                <NavLink to={`/${username}/parties/${character.party.id}`}>
-                  {character.party.name}
-                </NavLink>{" "}
-              </h3>
-            ) : (
-              <h3>Party: (Loading...)</h3>
-            )}
-            {this.state.loaded ? (
-              <h3>
-                Campaign:{" "}
-                <NavLink to={`/${username}/campaigns/${character.campaign.id}`}>
-                  {character.campaign.name}
-                </NavLink>
-              </h3>
-            ) : (
-              <h3>Campaign: (Loading...)</h3>
-            )}
-          </div>
-
-          <div className="character-page-backstory">
-            <h3>Backstory</h3>
-            {this.state.loaded ? (
-              renderCharacterBackstory(character)
-            ) : (
-              <p>(Loading...)</p>
-            )}
-          </div>
+          <Menu widths={4} fluid stackable>
+            <Menu.Item link>XP: {character.experience}</Menu.Item>
+            <Menu.Item link>Level: {character.level}</Menu.Item>
+            <Menu.Item link>
+              Health: {character.character_class.health[character.level]}
+            </Menu.Item>
+            <Menu.Item link>Gold: {character.gold}</Menu.Item>
+          </Menu>
         </div>
-
-        <div className="character-page-perks">
-          <h3>Perks</h3>
-          <p>(Stretch Goal)</p>
+        {/* contains links to the campaign and party page */}
+        <div>
+          <h3>
+            Party:{" "}
+            <NavLink to={`/${username}/parties/${character.party.id}`}>
+              {character.party.name}
+            </NavLink>{" "}
+            <br />
+            Campaign:{" "}
+            <NavLink to={`/${username}/campaigns/${character.campaign.id}`}>
+              {character.campaign.name}
+            </NavLink>
+          </h3>
         </div>
-
-        <div className="character-page-items">
-          <h3>Items</h3>
-          <p>(Stretch Goal)</p>
-        </div>
-
-        <div className="character-page-checks">
-          <h3>Checks</h3>
-          {this.state.loaded ? (
-            <p>{character.checkmarks}</p>
-          ) : (
-            <p>(Stretch Goal)</p>
-          )}
-        </div>
-
-        <div className="character-page-notes">
-          <h3>Notes</h3>
+        {/* TODO make the notes field editable and add ability to save note */}
+        {/* requires adding a column to the Character table */}
+        <div>
+          <h2>Notes</h2>
           <p>lorum lopsum ipsum </p>
         </div>
       </div>
-    );
-  }
-}
-
-function renderCharacterHeader(character) {
-  return (
-    <div className="character-page-header">
-      <div className="character-page-icon">
-        <img
-          src={character.character_class.img_icon}
-          alt={`${character.character_class.name} icon`}
-          style={{ height: 50, width: 50 }}
-        />
+      {/* TODO create the character perks game data and render depending on character class */}
+      <div className="character-page__perks">
+        <h2>Perks</h2>
+        <p>(Stretch Goal)</p>
       </div>
-      <div className="character-page-name">
-        <h3>{character.name}</h3>
+      {/* TODO create the game items (and store, etc) and render purchased items here */}
+      <div className="character-page__items">
+        <h2>Items</h2>
+        <p>(Stretch Goal)</p>
       </div>
-    </div>
-  );
-}
-
-function renderCharacterStats(character) {
-  return (
-    <div className="character-page-stats">
-      <Menu widths={4} fluid>
-        <Menu.Item link>XP: {character.experience}</Menu.Item>
-        <Menu.Item link>Level: {character.level}</Menu.Item>
-        <Menu.Item link>
-          Health: {character.character_class.health[character.level]}
-        </Menu.Item>
-        <Menu.Item link>Gold: {character.gold}</Menu.Item>
-      </Menu>
+      {/* TODO add check boxes that render based on the amount of checks */}
+      {/* TODO update character model when checking and unchecking checks */}
+      <div className="character-page__alt-stats">
+        <h2>Battle Goals</h2>
+        <p>{character.checkmarks} Checkmarks</p>
+      </div>
+      <div className="character-page__backstory">
+        <h2>Backstory</h2>
+        {renderCharacterBackstory(character)}
+      </div>
     </div>
   );
 }
 
 function renderCharacterBackstory(character) {
+  // takes in a character object and splits the description into two paragraphs on '\n\n'
   let description = character.character_class.description.split("\n\n");
   return (
     <>
       <p>{description[0]}</p>
+      <br />
       <p>{description[1]}</p>
     </>
   );
