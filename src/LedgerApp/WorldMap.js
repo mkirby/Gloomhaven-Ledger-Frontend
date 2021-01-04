a0b16782013462c1f19c9bae3d4826e63f130c0c
Matthew Kirby <matt.kirby@yahoo.com>
2021-01-04 13:34:55 -0500

    added a table oof scenario details and rough pagination of table data

--- src/LedgerApp/WorldMap.js
+++ src/LedgerApp/WorldMap.js
@@ -1,14 +1,57 @@
 import React from "react";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import scenarios from "./scenarios.json";
 
-import { Image, Button } from "semantic-ui-react";
+import { Image, Menu, Checkbox, Table, Icon } from "semantic-ui-react";
 import "./WorldMap.css";
 
 class WorldMap extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      scenarioPage: 1,
+      maxScenarioPage: Math.ceil(scenarios.length / 15),
+    };
   }
+
+  renderScenarioNames = (scenarios) => {
+    const { scenarioPage } = this.state;
+    // 15 items per page: index 0-14, 15-29, etc
+    // page * 10 - 10 = starting index: page 5 = 40
+    // page * 10 - 1 = ending index: page 5 = 49
+    let startingIndex = scenarioPage * 15 - 15;
+    let endingIndex = scenarioPage * 15 - 1;
+
+    return scenarios.map((scenario, index) => {
+      if (index >= startingIndex && index <= endingIndex) {
+        return (
+          <Table.Row key={scenario.id}>
+            <Table.Cell>{scenario.id}.</Table.Cell>
+            <Table.Cell>{scenario.coords}</Table.Cell>
+            <Table.Cell>
+              <Checkbox />
+            </Table.Cell>
+            <Table.Cell>
+              <Checkbox />
+            </Table.Cell>
+            <Table.Cell>{scenario.name}</Table.Cell>
+          </Table.Row>
+        );
+      }
+      return null;
+    });
+  };
+
+  changePage = (value) => {
+    const { maxScenarioPage, scenarioPage } = this.state;
+    let newPage = scenarioPage + value;
+    if (newPage < 1) {
+      newPage = 1;
+    } else if (newPage > maxScenarioPage) {
+      newPage = maxScenarioPage;
+    }
+    this.setState({ scenarioPage: newPage }, () => console.log(this.state));
+  };
 
   render() {
     return (
@@ -26,15 +69,45 @@ class WorldMap extends React.Component {
         </div>
         <div className="world__scenarios">
           <h1>Scenarios</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+          <Table
+            celled
+            striped
+            collapsing
+            size="small"
+            compact="very"
+            color="orange"
+          >
+            <Table.Header>
+              <Table.Row textAlign="center">
+                <Table.HeaderCell>Id</Table.HeaderCell>
+                <Table.HeaderCell>
+                  <Icon name="location arrow" />
+                </Table.HeaderCell>
+                <Table.HeaderCell>
+                  <Icon name="unlock" />
+                </Table.HeaderCell>
+                <Table.HeaderCell>
+                  <Icon name="check" />
+                </Table.HeaderCell>
+                <Table.HeaderCell>Name</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>{this.renderScenarioNames(scenarios)}</Table.Body>
+            <Table.Footer>
+              <Table.Row textAlign="center">
+                <Table.HeaderCell colSpan="5">
+                  <Menu pagination>
+                    <Menu.Item as="a" icon onClick={() => this.changePage(-1)}>
+                      <Icon name="chevron left" />
+                    </Menu.Item>
+                    <Menu.Item as="a" icon onClick={() => this.changePage(1)}>
+                      <Icon name="chevron right" />
+                    </Menu.Item>
+                  </Menu>
+                </Table.HeaderCell>
+              </Table.Row>
+            </Table.Footer>
+          </Table>
         </div>
       </div>
     );
