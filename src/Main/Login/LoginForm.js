2935cebeb365c822b8eb0c489d6f154b1568c672
Matthew Kirby <matt.kirby@yahoo.com>
2021-02-05 15:59:59 -0500

    updated login page warnings and validations

--- src/Main/Login/LoginForm.js
+++ src/Main/Login/LoginForm.js
@@ -1,15 +1,8 @@
 import React from "react";
-import { Button, Form } from "semantic-ui-react";
+import { Button, Form, Message } from "semantic-ui-react";
 import { connect } from "react-redux";
 
 import { userActions } from "../../_actions/userActions";
-
-function ValidationMessage(props) {
-  if (!props.valid) {
-    return <div>{props.message}</div>;
-  }
-  return null;
-}
 
 class LoginForm extends React.Component {
   state = {
@@ -18,7 +11,6 @@ class LoginForm extends React.Component {
     password: "",
     passwordValid: false,
     formValid: false,
-    errorMsg: {},
   };
 
   validateForm = () => {
@@ -33,16 +25,14 @@ class LoginForm extends React.Component {
   };
 
   validateUsername = () => {
-    const { username } = this.state;
-    let usernameValid = true;
-    let errorMsg = { ...this.state.errorMsg };
+    // validates the username being inputted into the controlled form
+    let usernameValid = this.state.username.length > 2;
+    // length of 3 or more characters
 
-    if (username.length < 3) {
-      usernameValid = false;
-      errorMsg.username = "Usernames must be at least 3 characters long";
+    // only re-set state if the value has changed
+    if (usernameValid !== this.state.usernameValid) {
+      this.setState({ usernameValid }, this.validateForm);
     }
-
-    this.setState({ usernameValid, errorMsg }, this.validateForm);
   };
 
   updatePassword = (password) => {
@@ -50,43 +40,38 @@ class LoginForm extends React.Component {
   };
 
   validatePassword = () => {
+    // validate the password being inputted into the controlled form
     const { password } = this.state;
-    let passwordValid = true;
-    let errorMsg = { ...this.state.errorMsg };
 
-    if (password.length < 8) {
-      passwordValid = false;
-      errorMsg.password = "Password must be a least 8 characters long";
-    } else if (!/\d/.test(password)) {
-      passwordValid = false;
-      errorMsg.password = "Password must contain at least one digit";
-    } else if (!/[!@#$%^&*]/.test(password)) {
-      passwordValid = false;
-      errorMsg.password =
-        "Password must contain at least one special character: !@#$%^&*";
+    // if the password meets params then it's valid
+    let passwordValid =
+      password.length > 7 && /\d/.test(password) && /[!@#$%^&*]/.test(password);
+    // length of 8 or more characters
+    // contains 1+ digits
+    // contains 1+ special characters
+
+    // only re-set state if the value has changed
+    if (passwordValid !== this.state.passwordValid) {
+      this.setState({ passwordValid }, this.validateForm);
     }
-    this.setState({ passwordValid, errorMsg }, this.validateForm);
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
     const { username, password } = this.state;
-    if (username && password) {
-      this.props.login(username, password);
-    }
+    this.props.login(username, password);
     this.setState({
       username: "",
       usernameValid: false,
       password: "",
       passwordValid: false,
       formValid: false,
-      errorMsg: {},
     });
   };
 
   render() {
     return (
-      <Form style={{ width: "400px" }}>
+      <Form style={{ width: "400px" }} warning>
         <Form.Field>
           <label>Username</label>
           <input
@@ -96,11 +81,8 @@ class LoginForm extends React.Component {
             value={this.state.username}
             onChange={(e) => this.updateUsername(e.target.value)}
           />
-          <ValidationMessage
-            valid={this.state.usernameValid}
-            message={this.state.errorMsg.username}
-          />
         </Form.Field>
+
         <Form.Field>
           <label>Password</label>
           <input
@@ -110,26 +92,34 @@ class LoginForm extends React.Component {
             value={this.state.password}
             onChange={(e) => this.updatePassword(e.target.value)}
           />
-          <ValidationMessage
-            valid={this.state.passwordValid}
-            message={this.state.errorMsg.password}
-          />
         </Form.Field>
-        <Button
-          type="submit"
-          disabled={!this.state.formValid}
-          onClick={this.handleSubmit}
-        >
-          Login
-        </Button>
+
+        <Button type="submit" onClick={this.handleSubmit} content="Login" />
+
+        <Message warning>
+          <Message.Header>Login Reminders</Message.Header>
+          <Message.List>
+            <Message.Item>
+              Username must be at least 3 characters long
+            </Message.Item>
+            <Message.Item>
+              Password must be a least 8 characters long
+            </Message.Item>
+            <Message.Item>
+              Password must contain at least one digit
+            </Message.Item>
+            <Message.Item>
+              Password must contain at least one special character: !@#$%^&*
+            </Message.Item>
+          </Message.List>
+        </Message>
       </Form>
     );
   }
 }
 
 function mapStateToProps(state) {
-  const { loggingIn } = state.authentication;
-  return { loggingIn };
+  return {};
 }
 
 const actionCreators = {
