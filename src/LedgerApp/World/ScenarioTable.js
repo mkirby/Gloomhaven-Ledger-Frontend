2f7128ec1c62ad006491b5bbdeaa45a0b7e74775
Matthew Kirby <matt.kirby@yahoo.com>
2021-03-15 20:55:27 -0400

    Updated world page scenario table conditional rendering based on checkboxes

--- src/LedgerApp/World/ScenarioTable.js
+++ src/LedgerApp/World/ScenarioTable.js
@@ -3,6 +3,60 @@ import scenarios from "./scenarios.json";
 import { Menu, Checkbox, Table, Icon } from "semantic-ui-react";
 import "./ScenarioTable.css";
 
+function renderScenarioRows(scenarios, scenarioPage) {
+  // 15 items per page: index 0-14, 15-29, etc
+  // page * 15 - 15 = starting index: page 4 = 50
+  // page * 15 - 1 = ending index: page 4 = 59
+  let startingIndex = scenarioPage * 15 - 15;
+  let endingIndex = scenarioPage * 15 - 1;
+
+  return scenarios.map((scenario, index) => {
+    let [unlocked, toggleUnlocked] = useState(false);
+    let [completed, toggleCompleted] = useState(false);
+
+    if (index >= startingIndex && index <= endingIndex) {
+      return (
+        <Table.Row key={scenario.id} positive={completed}>
+          <Table.Cell>{scenario.id}.</Table.Cell>
+          <Table.Cell>{scenario.coords}</Table.Cell>
+          <Table.Cell>
+            {/*
+              scenario unlocked checkbox
+              this checkbox is disabled once a user has marked the scenario as complete
+            */}
+            <Checkbox
+              onChange={() => toggleUnlocked(!unlocked)}
+              checked={unlocked}
+              disabled={completed}
+            />
+          </Table.Cell>
+          <Table.Cell>
+            {/*
+              scenario completed checkbox
+              this checkbox is disabled while the scenario is locked - ie. unlocked = false
+            */}
+            <Checkbox
+              onChange={() => toggleCompleted(!completed)}
+              checked={completed}
+              disabled={!unlocked}
+            />
+          </Table.Cell>
+          <Table.Cell>
+            {unlocked ? (
+              scenario.name
+            ) : (
+              <p style={{ color: "transparent", backgroundColor: "lightgrey" }}>
+                {scenario.name}
+              </p>
+            )}
+          </Table.Cell>
+        </Table.Row>
+      );
+    }
+    return null;
+  });
+}
+
 function ScenarioTable() {
   const [scenarioPage, changeScenarioPage] = useState(1);
   const maxScenarioPage = Math.ceil(scenarios.length / 15);
@@ -17,36 +71,8 @@ function ScenarioTable() {
     changeScenarioPage(newPage);
   };
 
-  const renderScenarioNames = (scenarios) => {
-    // 15 items per page: index 0-14, 15-29, etc
-    // page * 15 - 15 = starting index: page 4 = 50
-    // page * 15 - 1 = ending index: page 4 = 59
-    let startingIndex = scenarioPage * 15 - 15;
-    let endingIndex = scenarioPage * 15 - 1;
-
-    return scenarios.map((scenario, index) => {
-      if (index >= startingIndex && index <= endingIndex) {
-        return (
-          <Table.Row key={scenario.id}>
-            <Table.Cell>{scenario.id}.</Table.Cell>
-            <Table.Cell>{scenario.coords}</Table.Cell>
-            <Table.Cell>
-              <Checkbox />
-            </Table.Cell>
-            <Table.Cell>
-              <Checkbox />
-            </Table.Cell>
-            <Table.Cell>{scenario.name}</Table.Cell>
-          </Table.Row>
-        );
-      }
-      return null;
-    });
-  };
-
   return (
     <div className="world__scenarios">
-      <h1>Scenarios</h1>
       <Table
         striped
         collapsing
@@ -56,6 +82,11 @@ function ScenarioTable() {
         color="orange"
       >
         <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell colSpan="5" textAlign="center">
+              <h1>Scenarios</h1>
+            </Table.HeaderCell>
+          </Table.Row>
           <Table.Row textAlign="center">
             <Table.HeaderCell>Id</Table.HeaderCell>
             <Table.HeaderCell>
@@ -70,7 +101,7 @@ function ScenarioTable() {
             <Table.HeaderCell>Name</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
-        <Table.Body>{renderScenarioNames(scenarios)}</Table.Body>
+        <Table.Body>{renderScenarioRows(scenarios, scenarioPage)}</Table.Body>
         <Table.Footer>
           <Table.Row textAlign="center">
             <Table.HeaderCell colSpan="5">
